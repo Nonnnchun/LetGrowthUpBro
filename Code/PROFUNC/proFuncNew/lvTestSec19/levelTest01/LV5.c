17fd4ee895c80a3bd89b8ae22394981fd302047e
Nonnnchern <chun990th.one@gmail.com>
2024-10-18 18:34:33 +0700

    LevelTest
    add folders

---
+++ Code/PROFUNC/proFuncNew/lvTestSec19/levelTest01/LV5.c
@@ -0,0 +1,10 @@
+#include <string.h>
+#include <stdio.h>
+
+void palindrom(int argc, char *c){
+   
+}
+
+int main(){
+   return 0;
+}
---
+++ Code/PROFUNC/proFuncNew/lvTestSec20/levelTest02/LV1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+   int num1, num2;
+   printf("Enter two integers: ");
+   scanf("%d %d", &num1, &num2);
+   int sum = num1+num2;
+
+   printf("sum of two number is : %d\n", sum);
+}
