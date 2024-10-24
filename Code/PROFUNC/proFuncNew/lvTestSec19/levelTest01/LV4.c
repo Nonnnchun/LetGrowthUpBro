c9cee75feefd2def5b2ee0ecc9eaed38eae47852
Nonnnchern <chun990th.one@gmail.com>
2024-10-25 04:45:09 +0700

    little

--- Code/PROFUNC/proFuncNew/lvTestSec19/levelTest01/LV4.c
+++ Code/PROFUNC/proFuncNew/lvTestSec19/levelTest01/LV4.c
@@ -4,33 +4,49 @@
 int main(){
    char a[100];
    char b[100];
-   printf("Keyword: ");
-   scanf("%s", a);
-   getchar();
-
    printf("Data: ");
-   scanf("%[^\n]", b);
+   scanf("%[^\n]", a);
    getchar();
 
+   // NOTE : fgets(a,sizeof(a),stdin);
+   printf("Keyword: ");
+   scanf("%s", b);
+   getchar();
+
+   int index = 0;
    int count = 0;
-   for(int i = 0; i <strlen(b); i++){
-      // NOTE : strstr is searches the string haystack for the first occurrence of the substring needle.
-      // SECTION - Success: A pointer to the first occurrence of the needle in the haystack is returned. If needle is found within haystack, strstr returns a pointer to the beginning of the first occurrence of needle.
-      // SECTION - Failure: If needle is not found within haystack, strstr returns NULL.
-      if(strstr(&b[i], a)!= NULL){
+   for(int i = 0; i <strlen(a); i++){
+      if (a[i] == b[index]){
+         for (int j = 0; j < strlen(b); j++){
+            if (a[j] != b[index]){
+               break;
+            } else if(a[j] == b[index]){
+                index++;
+            }
+         }
+      }
+      if (index == strlen(b) - 1){
          count++;
-         i += strlen(a);
+         index = 0;
       }
-      /* else if(b[i] ==' ' && b[i]!= '\0'){
-         i++;
-      }
-      else if(b[i] == '\0'){
-         break;
-      }
-      else{
-         i++;
-      } */
-   }
-   printf("Keyword '%s' found %d times in the given data.\n", a, count);
+   printf("Keyword '%s' found %d times in the given data.\n", b, count);
    return 0;
+   }
 }
+
+// NOTE : strstr is searches the string haystack for the first occurrence of the substring needle.
+// SECTION - Success: A pointer to the first occurrence of the needle in the haystack is returned. If needle is found within haystack, strstr returns a pointer to the beginning of the first occurrence of needle.
+// SECTION - Failure: If needle is not found within haystack, strstr returns NULL.
+/* if(strstr(&b[i], a)!= NULL){
+   count++;
+   i += strlen(a);
+} */
+/* else if(b[i] ==' ' && b[i]!= '\0'){
+   i++;
+}
+else if(b[i] == '\0'){
+   break;
+}
+else{
+   i++;
+} */
--- Code/PROFUNC/proFuncNew/lvTestSec19/levelTest01/LV5.c
+++ Code/PROFUNC/proFuncNew/lvTestSec19/levelTest01/LV5.c
@@ -1,10 +1,28 @@
 #include <string.h>
 #include <stdio.h>
 
-void palindrom(int argc, char *c){
-   
-}
 
 int main(){
+   char input[100];
+   char str[100];
+   int index = 0;
+
+   printf("Input string : ");
+   fgets(input, 100, stdin);
+   for (int i = 0; i < strlen(input); i++){
+      if ((input[i] >= 'A' && input[i] <= 'Z') || (input[i] >= 'a' && input[i] <= 'z')){
+         str[index] = input[i];
+         index++;
+      }
+   }
+   for (int i = 0; i < index; i++){
+      if (str[i] == str[index - 1 - i] || str[i] == str[index - 1 - i] + 32 || str[i] == str[index - 1 - i] - 32){
+         continue;
+      } else {
+         printf("Not palindrome\n");
+         return 0;
+      }
+   }
+   printf("Palindrome\n");
    return 0;
 }
